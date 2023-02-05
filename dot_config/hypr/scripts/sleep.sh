3954e87210eed78300d029df3af40f829822d719
Eduardo Alves <eduardoalves1988@gmail.com>
2023-02-05 19:36:52 +0000

    update sleep behavior

--- dot_config/hypr/scripts/sleep.sh
+++ dot_config/hypr/scripts/sleep.sh
@@ -2,9 +2,9 @@
 
 swayidle -w \
         before-sleep "$HOME/.config/hypr/scripts/lock.sh" \
-        timeout 160 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
-            resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-        timeout 300 "$HOME/.config/hypr/scripts/lock.sh & sleep 0.1 && hyprctl dispatch dpms off" \
+        timeout 50 'temp=$(brightnessctl g); brightnessctl set $((temp / 4))' \
+            resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 4))' \
+        timeout 60 "$HOME/.config/hypr/scripts/lock.sh & sleep 0.5 && hyprctl dispatch dpms off" \
             resume 'hyprctl dispatch dpms on' \
         timeout 600 'systemctl suspend' \
             resume 'hyprctl dispatch dpms on'
