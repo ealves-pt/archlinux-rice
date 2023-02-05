173a61becc58d95e856268341e1f65c108d32426
Eduardo Alves <eduardoalves1988@gmail.com>
2023-02-05 22:45:01 +0000

    update spotify color

--- setup/spotify.sh
+++ setup/spotify.sh
@@ -12,7 +12,7 @@ spicetify backup
 
 # setup config
 spicetify config current_theme catppuccin-mocha
-spicetify config color_scheme blue
+spicetify config color_scheme lavender
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
 spicetify config extensions catppuccin-mocha.js
 
