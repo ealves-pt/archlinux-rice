8de4a8d6f4bb8683a7aef369ac6b7f194e0db2e9
Eduardo Alves <eduardoalves1988@gmail.com>
2023-02-05 13:33:28 +0000

    add Hyprland config

---
+++ dot_config/hypr/scripts/lock.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+swaylock \
+    --indicator-radius 50 \
+    --indicator-thickness 5 \
+    --inside-color cba6f7 \
+    --line-color 00000000 \
+    --ring-color cba6f7 \
+    --inside-ver-color 89b4fa\
+    --line-ver-color 00000000 \
+    --ring-ver-color 89b4fa \
+    --inside-wrong-color f38ba8 \
+    --line-wrong-color 00000000 \
+    --ring-wrong-color f38ba8 \
+    --inside-clear-color a6e3a1 \
+    --line-clear-color 00000000 \
+    --ring-clear-color a6e3a1 \
+    --key-hl-color 1e1e2e \
+    --bs-hl-color eba0ac \
+    --text-color 11111b \
+    --text-caps-lock-color 11111b \
+    --separator-color 00000000
---
+++ dot_config/hypr/scripts/logout.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wlogout --protocol layer-shell -b 5 -T 500 -B 500
